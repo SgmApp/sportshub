5c736e9a1a753da9a9920d46afd7bb60de99f7e6
SgmApp <shafikpin@gmail.com>
2026-07-06 19:47:19 +0530

    Create parseCustom.js

---
+++ parse/parseCustom.js
@@ -0,0 +1,66 @@
+module.exports = function (data) {
+
+    const competitions = [];
+    const games = [];
+
+    // ഇവിടെ നിങ്ങളുടെ Custom API JSON
+    // standard format ലേക്ക് convert ചെയ്യുക.
+
+    /*
+    games.push({
+
+        id: ...,
+
+        competitionId: ...,
+
+        competitionDisplayName: ...,
+
+        homeCompetitor: {
+
+            id: ...,
+
+            name: ...,
+
+            score: ...
+
+        },
+
+        awayCompetitor: {
+
+            id: ...,
+
+            name: ...,
+
+            score: ...
+
+        },
+
+        venue: {
+
+            name: ...
+
+        },
+
+        streamUrl: ...,
+
+        statusText: ...,
+
+        shortStatusText: ...,
+
+        startTime: ...,
+
+        homeLogo: ...,
+
+        awayLogo: ...
+
+    });
+    */
+
+    return {
+
+        competitions,
+        games
+
+    };
+
+};
