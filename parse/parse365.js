a65fc7dfd723a4d523f90d439b99ad76cc08d8a9
SgmApp <shafikpin@gmail.com>
2026-07-06 20:14:57 +0530

    Update parse365.js

--- parse/parse365.js
+++ parse/parse365.js
@@ -1,10 +1,60 @@
 module.exports = function (data) {
 
+    const competitions = [];
+    const games = [];
+
+    (data.competitions || []).forEach(c => {
+
+        competitions.push({
+            id: c.id,
+            name: c.name
+        });
+
+    });
+
+    (data.games || []).forEach(g => {
+
+        games.push({
+
+            gameId: g.id,
+
+            competitionId: g.competitionId,
+
+            league: g.competitionDisplayName || "",
+
+            home: g.homeCompetitor?.name || "",
+
+            away: g.awayCompetitor?.name || "",
+
+            score: "",
+
+            status: "",
+
+            shortStatus: "",
+
+            streamUrl: "",
+
+            stadium: g.venue?.name || "",
+
+            date: "",
+
+            time: "",
+
+            matchTimeMillis: 0,
+
+            homeLogo: "",
+
+            awayLogo: ""
+
+        });
+
+    });
+
     return {
 
-        competitions: data.competitions || [],
+        competitions,
 
-        games: data.games || []
+        games
 
     };
 
